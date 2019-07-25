5cce2cfbde7181a1b1fe56283a0177b363cac6ec
k3ntako <kentarokaneki@gmail.com>
2019-07-25 16:07:07 -0400

    Nav bar fix and design

--- app/javascript/packs/react/components/Nav/index.jsx
+++ app/javascript/packs/react/components/Nav/index.jsx
@@ -8,11 +8,14 @@ import styles from './index.module.css'
 
 export default (props) => {
   return <Navbar id={styles.navBar} expand="sm">
-    <Navbar.Brand className={styles.navLinks} href="/">CafeTako</Navbar.Brand>
+
+    <Navbar.Brand>
+      <Link className={styles.navLinks} to="/">CafeTako</Link>
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link href="/locations/new">Add Location</Nav.Link>
+        <Link className={styles.navLinks} to="/locations/new">Add Location</Link>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
