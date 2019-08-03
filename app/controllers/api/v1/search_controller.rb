617a51eef248e8eb7204ed3b0b68f91418e6a31e
k3ntako <kentarokaneki@gmail.com>
2019-08-03 02:22:31 -0400

    Search include locations

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -2,8 +2,12 @@ class Api::V1::SearchController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def search
-    t = Location.arel_table
-    search_results = Location.where(t[:name].matches("%#{search_params[:search].downcase}%"))
+    location_table = Location.arel_table
+    location_results = Location.where(location_table[:name].matches("%#{search_params[:search].downcase}%"))
+    chain_table = Location.arel_table
+    chain_results = Location.where(chain_table[:name].matches("%#{search_params[:search].downcase}%"))
+
+    search_results = chain_results + location_results
 
     render json: search_results
   end
