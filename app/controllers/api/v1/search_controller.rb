daa36cc08244ddb6ee625e0f4a098bf1b5a84770
k3ntako <kentarokaneki@gmail.com>
2019-08-03 19:41:08 -0400

    Distance calc on query

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -7,10 +7,10 @@ class Api::V1::SearchController < ApplicationController
 
     distance_formula = "( 3959 * acos( cos( radians(#{user_lat}) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(#{user_lng}) ) + sin( radians(#{user_lat}) ) * sin( radians( lat ) ) ) )"
 
-    close_locations = Location.where("#{distance_formula} < 25")
+    close_locations = Location.select("*, #{distance_formula} AS distance").where("#{distance_formula} < 25").order(:distance)
     search_results = close_locations.chain_location_search(search_params[:search].downcase)
 
-    render json: search_results, search_params: search_params
+    render json: search_results
   end
 
   private
