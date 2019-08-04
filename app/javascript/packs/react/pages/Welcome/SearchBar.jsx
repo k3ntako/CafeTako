f84d45a520698bc9c8d33f92d934df9755dad532
k3ntako <kentarokaneki@gmail.com>
2019-08-03 20:07:06 -0400

    Design updates

--- app/javascript/packs/react/pages/Welcome/SearchBar.jsx
+++ app/javascript/packs/react/pages/Welcome/SearchBar.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
-import SearchBox from '../../components/GoogleMapsForm/SearchBox';
+import Button from 'react-bootstrap/Button';
 
+import SearchBox from '../../components/GoogleMapsForm/SearchBox';
 import Location from '../../models/Location';
 import styles from './index.module.css';
 
@@ -11,7 +12,8 @@ export default class SearchBar extends Component{
     super(props);
     this.state = {
       search: "",
-      place: null,
+      lat: null,
+      lng: null,
     }
   }
 
@@ -22,25 +24,36 @@ export default class SearchBar extends Component{
   }
 
   submit = () => {
-    const place = this.state.place;
-    const lat = place && place.geometry && place.geometry.location && place.geometry.location.lat();
-    const lng = (lat === 0 || typeof lat === "number") && place.geometry.location.lng();
+    if( this.isValid() ){
+      const { search, lat, lng } = this.state;
 
-    if( lat && lng && this.state.search.trim() ){
-      Location.search( this.state.search, lat, lng )
+      Location.search( search, lat, lng )
         .then(results => this.props.updateSearchResults(results));
     }
   }
 
+  isValid(){
+    const { search, lat, lng } = this.state;
+
+    return search && typeof lat === "number" && ( lat || lat === 0 ) &&
+      typeof lng === "number" && ( lng || lng === 0 );
+  }
+
   onPlacesChanged = (place) => {
-    this.setState({ place })
+    const lat = place && place.geometry && place.geometry.location && place.geometry.location.lat();
+    const lng = typeof lat === "number" && (lat || lat === 0) && place.geometry.location.lng();
+
+    if( (lat || lat === 0) && (lng || lng === 0) ){
+      this.setState({ lat, lng });
+    }else{
+      console.error("Unable to save coordinates.");
+    }
   }
 
   render(){
     return <div className={styles.searchBar}>
       <InputGroup size="lg">
-
-        <InputGroup.Prepend onClick={this.submit}>
+        <InputGroup.Prepend>
           <InputGroup.Text>
             <i className="fas fa-coffee"></i>
           </InputGroup.Text>
@@ -51,12 +64,19 @@ export default class SearchBar extends Component{
           onChange={(e) => this.setState({ search: e.target.value })}
           onKeyPress={this.handleKeyPress}/>
 
-        <InputGroup.Prepend onClick={this.submit}>
+        <InputGroup.Prepend>
           <InputGroup.Text>
             <i className="fas fa-map-marker-alt"></i>
           </InputGroup.Text>
         </InputGroup.Prepend>
         <SearchBox onPlacesChanged={this.onPlacesChanged} placeholder="Location"/>
+        <InputGroup.Append
+          onClick={this.submit}
+          className={this.isValid() ? styles.valid : ""}>
+          <InputGroup.Text>
+            <i className="fas fa-search"></i>
+          </InputGroup.Text>
+        </InputGroup.Append>
       </InputGroup>
     </div>
   }
